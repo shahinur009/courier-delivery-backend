fbd5430c78811dbe6a8397846b64bc57d6e37f4f
khalid586 <khalidcomilla58@gmail.com>
2024-06-03 08:46:01 +0600

    user registration done

--- index.js
+++ index.js
@@ -1,18 +1,99 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const helmet = require('helmet');
+
 const port = process.env.PORT || 5007;
 
 const app = express();
-const corsOptions = [
-    'http://localhost:5173',
-    'http://localhost:5174',
-]
+const corsOptions = {
+    origin:[
+        'http://localhost:5173',
+        'http://localhost:5174',
+    ],
+    optionsSuccessStatus: 200,
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+
+}
 
 app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(helmet({
+    crossOriginOpenerPolicy: { policy: "same-origin" },
+    crossOriginEmbedderPolicy: { policy: "require-corp" }
+  }));
+
+require('dotenv').config();
+const uri = process.env.MONGO_URL
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+async function run(){
+    try{
+        const usersCollection = client.db('usersCollection').collection('users');
+        const ordersCollection = client.db('ordersCollection').collection('orders');
+        
+        app.get('/orders',async(req,res)=>{
+            const {name,email} = req.query;
+            console.log(name,email)
+            const query = {
+                placedBy:{
+                    name,
+                    email,
+                }
+            }
+            const result = await ordersCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.post('/place_order', async (req, res) => {
+            try {
+              const order = req.body;
+              const result = await ordersCollection.insertOne(order);
+              res.send(result);
+            } catch (error) {
+              console.error('Error placing order:', error);
+              res.status(500).send('Error placing order');
+            }
+          });
+          
+        
+        app.post('/users',async(req,res) =>{
+            const {name,email} = req.body;
+            console.log(name,email);
+            const query = {
+                name,
+                email,
+                role:'user',
+            }
+            const exist = await usersCollection.findOne(query)
+            if(!exist){
+                const result = await usersCollection.insertOne(query);
+                res.send(result);
+            }
+        })
+
+    await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    }catch(error){
+        console.log(error)
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req,res)=>{
-    res.send('working')
+    res.send('working 1.0')
 })
 
 app.listen(port,()=>{
