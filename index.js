d4777ee69a33b30335f9b2ffcfcff4e49b0c5b0d
khalid586 <khalidcomilla58@gmail.com>
2024-06-08 19:06:09 +0600

    user role modification api endpoint done

--- index.js
+++ index.js
@@ -129,6 +129,16 @@ async function run(){
             )
             res.send(result);
         })
+        app.patch('/update/user/:id',async (req,res)=>{
+            const id = req.params.id;
+            const updatedData = req.body;
+            const result = await usersCollection.updateOne(
+                {_id:new ObjectId(id)},
+                {$set:updatedData}
+
+            )
+            res.send(result);
+        })
 
     await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
